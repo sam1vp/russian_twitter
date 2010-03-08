5df37cfc63b7ead0e343594b2f32aa189c3505f0
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2010-03-08 00:56:01 -0500

    modified column names in language migration

--- db/migrate/20100306184447_create_language_tables.rb
+++ db/migrate/20100306184447_create_language_tables.rb
@@ -3,7 +3,7 @@ class CreateLanguageTables < ActiveRecord::Migration
     create_table :languages do |t|
       t.string :iso_lang_code
       t.integer :tweet_id
-      t.boolean :is_reliable?
+      t.boolean :is_reliable
       t.float :confidence
     end
   end
