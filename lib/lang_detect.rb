f4aa9fe4bfa76705ff376a468892dd6e307fd156
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2010-03-07 21:18:25 -0500

    added lang_process_tweet and lang_all_tweets methods to lang_detect

--- lib/lang_detect.rb
+++ lib/lang_detect.rb
@@ -18,3 +18,36 @@ def lang_detect(text)
     data = response['responseData']
     return data
 end
+
+def lang_process_tweet(tweet)
+  if tweet.language
+    return tweet.language
+  else
+    begin
+      lang_data = lang_detect(tweet.text)
+      language = Language.new(:iso_lang_code => lang_data["language"], :tweet_id => tweet.id, :confidence => lang_data["confidence"], :is_reliable? => lang_data["isReliable"])
+      language.save
+      return tweet.language
+    rescue
+      puts "error processing tweet. moving on. Google data: #{lang_data.inspect}"
+    end
+  end
+end
+
+def lang_all_tweets
+   i = 1000
+   count = 0
+   loop do 
+     tweet_set = Tweet.all(:conditions => "id >= #{i - 1000} and id < #{i}")
+     if tweet_set.size == 0 
+       break
+     end
+     tweet_set.each do |tweet|
+       lang_process_tweet(tweet)
+       count += 1
+     end
+     i += 1000
+     puts "count = #{count}"
+   end
+   puts "Language Processing Complete. Detected language of #{count} tweets"
+end
