1c4d049366a608f813331955ba23675d1e68c0fd
Sam Gilbert <sam.o.gilbert@gmail.com>
2010-03-10 01:45:18 -0500

    added logging to pull_term; removed new_term method by incorporating it into pull_term method

--- lib/pull_terms.rb
+++ lib/pull_terms.rb
@@ -1,15 +1,14 @@
 terms = ['#russia','#ru', '#spb', '#rutwitter', '#moscow', '#msk', '#rostov', '#rnd', '#piter'] 
-new_terms = []
 
+$PORK_LOG = Pork::Log.new("#{File.dirname(__FILE__)}/../log/#{Time.now.strftime('%m.%d.%Y')}_pull_terms.log")
 terms.each do |t|
   $SAVER.rules = {'tags' => {'tag' => t}}
   latest_id = Tweet.tagged_with(t).map {|tweet| tweet.status_id}.max
-  Pork::Search.new(t,{:since_id => latest_id})
+  if latest_id 
+    Pork::Search.new(t,{:since_id => latest_id}).historical_pull
+  else
+    Pork::Search.new(t).historical_pull
+  end
   puts "completed #{t} pull"
 end
 
-new_terms.each do |t|
-  $SAVER.rules = {'tags' => {'tag' => t}}
-  Pork::Search.new(t).historical_pull
-  puts "completed #{t} pull"
-end
