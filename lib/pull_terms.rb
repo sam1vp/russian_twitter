d065e5fa34ac83d0be03671d14aae6ca544157c8
Sam Gilbert <sam.o.gilbert@gmail.com>
2010-03-10 01:03:44 -0500

    added new method for new terms

--- lib/pull_terms.rb
+++ lib/pull_terms.rb
@@ -1,8 +1,15 @@
-terms = ['#russia', '#ru', '#spb', '#rutwitter', '#moscow', '#msk', '#rostov', '#rnd', '#piter']
+terms = ['#russia'] 
+new_terms = ['#ru', '#spb', '#rutwitter', '#moscow', '#msk', '#rostov', '#rnd', '#piter']
 
 terms.each do |t|
   $SAVER.rules = {'tags' => {'tag' => t}}
   latest_id = Tweet.tagged_with(t).map {|tweet| tweet.status_id}.max
   Pork::Search.new(t,{:since_id => latest_id})
+  puts "completed #{t} pull"
+end
+
+new_terms.each do |t|
+  $SAVER.rules = {'tags' => {'tag' => t}}
+  Pork::Search.new(t)
   puts "completed #{t} pull"
 end
