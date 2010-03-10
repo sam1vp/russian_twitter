c2a8fb0b9fad2b51343f283f636512bffbbe2981
Sam Gilbert <sam.o.gilbert@gmail.com>
2010-03-09 20:13:33 -0500

    modified lang_detect to accomodate alchemy api

--- lib/lang_detect.rb
+++ lib/lang_detect.rb
@@ -2,22 +2,26 @@ require 'rubygems'
 require 'json'
 require 'net/http'
 
-DETECT_API_PARAMS = {'v'=>'1.0', 'q'=>''}
-DETECT_URL = 'http://ajax.googleapis.com/ajax/services/language/detect?'
-
+# commented out values are for the google api
+DETECT_API_PARAMS = {'outputMode' => "json",'apikey' => YAML::load(File.open("#{File.dirname(__FILE__)}/../config/pork_config.yml"))["alchemy_api_key"]}   #{'v'=>'1.0', 'q'=>''}
+DETECT_URL = 'http://access.alchemyapi.com/calls/text/TextGetLanguage?'  #'http://ajax.googleapis.com/ajax/services/language/detect?'
+TEXT_PARAM = 'text' # 'q'
+API = 'alchemy' #'google'
+API_DAILY_RATE = 30_000
 
 def lang_detect(text)
     detect_params = DETECT_API_PARAMS.dup
-    detect_params['q'] = URI.escape(text)
+    detect_params[TEXT_PARAM] = URI.escape(text)
     uri_encoded_params = ''
     detect_params.each do |k,v|
       uri_encoded_params << "#{k}=#{v}&"
     end
     url = DETECT_URL + uri_encoded_params.chop
     response = JSON.parse(Net::HTTP.get(URI.parse(url)))    
-    data = response['responseData']
-    return data
+    return response
 end
+
+
 
 def lang_process_tweet(tweet)
   if tweet.language
@@ -25,15 +29,20 @@ def lang_process_tweet(tweet)
   else
     begin
       lang_data = lang_detect(tweet.text)
-      language = Language.new(:iso_lang_code => lang_data["language"], :tweet_id => tweet.id, :confidence => lang_data["confidence"], :is_reliable => lang_data["isReliable"])
+      if API == 'google'
+        language = Language.new(:iso_lang_code => lang_data["language"], :tweet_id => tweet.id, :confidence => lang_data["confidence"], :is_reliable => lang_data["isReliable"])
+      elsif API == 'alchemy'
+        iso_code = lang_data["iso-639-1"]? lang_data["iso-639-1"] : lang_data["language"]
+        language = Language.new(:iso_lang_code => iso_code, :tweet_id => tweet.id, :is_reliable => true)
+      end
       language.save
-      return tweet.language
     rescue
-      puts "error processing tweet. moving on. Google data: #{lang_data.inspect}"
+      puts "error processing tweet. moving on. Data: #{lang_data.inspect}"
       if not lang_data
         sleep 600
       end
     end
+    return tweet.language
   end
 end
 
@@ -46,7 +55,10 @@ def lang_all_tweets
        break
      end
      tweet_set.each do |tweet|
+       start_time = Time.now
        lang_process_tweet(tweet)
+       pause_time = (API_DAILY_RATE/24.0/60.0/60.0 - (Time.now - start_time))
+       sleep pause_time if pause_time > 0
        count += 1
      end
      i += 1000
