e298c22677b7eb09eaeb207e62342ae601dfaa3d
Sam Gilbert <sam.o.gilbert@gmail.com>
2010-03-14 22:58:21 -0400

    modified manual_log and todo based on meetings; fixed bug in pull users that was causing more metadata than users to be created

--- lib/pull_users.rb
+++ lib/pull_users.rb
@@ -1,8 +1,6 @@
 require 'ruby-debug'
 Object.send :undef_method, :id
 
-auth_info = YAML::load(File.open("#{File.dirname(__FILE__)}/../config/pork_config.yml"))
-$AUTH = Pork::Auth.new(auth_info["twitter_auth"]["username"],auth_info["twitter_auth"]["password"])
 
 def pull_user(user, via)
   @user = Pork::TwitterUser.new(:twitter_screen_name => user)
@@ -10,9 +8,6 @@ def pull_user(user, via)
   if @user.db_object
     @db_id = @user.db_object.id 
     AccountMetadata.create(:acquired_via => via, :twitter_account_id => @db_id, :account_info_updated => Time.now)
-  else
-    debugger
-    nil
   end
 end
 
